60e3f5e52b9f9fbd50f4b22ff66214354bfaafc7
Kallen-Hsu <kallen.hsu82@gmail.com>
2023-09-14 00:13:38 +0800

    幫頁面新增icon，地圖點擊部分修改中

--- assets/js/pages/hair_care_clinic.js
+++ assets/js/pages/hair_care_clinic.js
@@ -1,140 +1,134 @@
+import 'default-passive-events';
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     let map;
-//     let currentPosition;
-//     let markers = [];
-//     const clinicsData = [
-//         { name: 'DCDC', location: { lat: 25.0422681, lng: 121.5424544 }, region: '台北市', type: '植髮診所' },
-//         { name: 'DCDC', location: { lat: 24.8153831, lng: 121.0217587 }, region: '新竹縣', type: '植髮診所' },
-//         { name: 'DCDC', location: { lat: 22.6610928, lng: 120.3075843 }, region: '高雄市', type: '植髮診所' },
-//         { name: 'DCDC', location: { lat: 24.1570995, lng: 120.6468087 }, region: '台中市', type: '植髮診所' },
-//         { name: 'DCDC', location: { lat: 25.0425358, lng: 121.5452807 }, region: '台北市', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 25.0278009, lng: 121.5195911 }, region: '台北市', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 23.8251276, lng: 119.4853344 }, region: '桃園市', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 24.8071615, lng: 120.7321717 }, region: '新竹縣', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 24.1568519, lng: 119.4270025 }, region: '台中市', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 24.1326835, lng: 117.4440798 }, region: '台南市', type: '植髮診所' },
-//         { name: 'i hair 風華御髮', location: { lat: 22.6647775, lng: 120.2978912 }, region: '高雄市', type: '植髮診所' },
-//         { name: '勻禾妍美學健康診所', location: { lat: 22.6856517, lng: 120.3071755 }, region: '高雄市', type: '美學診所' },
-//         { name: 'H&H 醫髮診所', location: { lat: 25.04589, lng: 120.3241616 }, region: '台北市', type: '頭髮護理診所' },
-//         { name: 'H&H 醫髮診所', location: { lat: 24.161399, lng: 119.4262235 }, region: '台中市', type: '頭髮護理診所' }
-//     ];
+let map;
+let markers = [];
+let infoWindow;
+const beaches = [
+    ["DCDC生髮診所總院", 25.0422681, 121.5424544, "植髮診所", "台北市", "https://dcdchair.com.tw/clinics/5", "台北市大安區大安路一段75巷21號2樓"],
+    ["DCDC生髮診所竹北院區", 24.8153831, 121.0217587, "植髮診所", "新竹縣", "https://dcdchair.com.tw/clinics/6", "新竹縣竹北市自強南路182號3樓"],
+    ["DCDC生髮診所高雄院區", 22.6610928, 120.3075843, "植髮診所", "高雄市", "https://dcdchair.com.tw/clinics/7", "高雄市左營區明誠二路180號2樓"],
+    ["DCDC生髮診所台北旗艦店", 25.0425358, 121.5452807, "植髮診所", "台北市", "https://dcdchair.com.tw/clinics/8", "台北市大安區敦化南路一段190巷12號"],
+    ["DCDC生髮診所台中院區", 24.157512, 120.6470562, "植髮診所", "台中市", "https://dcdchair.com.tw/clinics/11", "台中市西屯區文心路二段566之1號3樓之2"],
+    ["ihair 風華御髮台北古亭店", 25.0278009, 121.522166, "美學診所", "台北市", "https://www.ihairtransplantclinic.com/taipei/ihair-taipei", " 台北市大安區羅斯福路二段33號4樓"],
+    ["ihair 風華御髮桃園中正店", 24.9965827, 121.3093853, "美學診所", "桃園市", "https://www.ihairtransplantclinic.com/taoyuan/ihair-taoyuan", " 桃園市桃園區中正路271號3樓"],
+    ["ihair 風華御髮新竹竹北高鐵店", 24.8071615, 121.0370423, "美學診所", "新竹縣", "https://www.ihairtransplantclinic.com/hsinchu/ihair-hsinchu", "新竹縣竹北市高鐵二路38號一樓"],
+    ["ihair 風華御髮台中新國店", 24.1568519, 120.6464849, "美學診所", "台中市", "https://www.ihairtransplantclinic.com/taichung/ihair-taichung", "台中市西屯區市政北一路1號1樓 1-D2"],
+    ["ihair 風華御髮台南崇明店", 22.9750387, 120.2226316, "美學診所", "台南市", "https://www.ihairtransplantclinic.com/tainan/ihair-tainan", "台南市東區崇明路377-1號2樓"],
+    ["ihair 風華御髮高雄巨蛋店", 22.6647775, 120.3004661, "美學診所", "高雄市", "https://www.ihairtransplantclinic.com/kaohsiung/", "高雄市鼓山區文信路234號2樓"],
+    ["勻禾妍美學健康診所", 22.6856517, 120.3071755, "美學診所", "高雄市", "https://www.icarebeauty.com.tw/", "高雄市左營區文學路25號"],
+    ["H&H 醫髮診所", 25.04589, 121.5410691, "頭髮護理診所", "台北市", "https://www.2h.com.tw/", "台北市中山區復興南路一段36-9號9F"],
 
-//     const regionSelect = document.getElementById("Region");
-//     const clinicTypeSelect = document.getElementById("Clinic");
+];
+function initMap() {
+    const mapElement = document.getElementById("map");
+    const defaultPosition = { lat: 25.0422681, lng: 121.5424544 }; // 預設位置（台北市）
 
-//     // 檢查DOM元素是否存在
-//     if (regionSelect && clinicTypeSelect) {
-//         regionSelect.addEventListener("change", performSearch);
-//         clinicTypeSelect.addEventListener("change", performSearch);
-//     }
+    map = new google.maps.Map(mapElement, {
+        center: defaultPosition,
+        zoom: 16,
+    });
+    infoWindow = new google.maps.InfoWindow();
 
-//     initMap();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                const currentPosition = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                initializeMap(currentPosition);
+            },
+            function (error) {
+                console.error("獲取地理位置失敗:", error);
+                initializeMap(defaultPosition); // 使用預設位置
+            }
+        );
+    } else {
+        console.error("瀏覽器不支持地理位置");
+        initializeMap(defaultPosition); // 使用預設位置
+    }
 
-// function initMap() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(
-//             function (position) {
-//                 currentPosition = {
-//                     lat: position.coords.latitude,
-//                     lng: position.coords.longitude,
-//                 };
-//                 initializeMap(currentPosition);
-//             },
-//             function (error) {
-//                 console.error("獲取地理位置失敗:", error);
-//                 initializeMap({ lat: 25.0422681, lng: 121.5424544 });
-//             }
-//         );
-//     } else {
-//         console.error("瀏覽器不支持地理位置");
-//         initializeMap({ lat: 25.0422681, lng: 121.5424544 });
-//     }
-// }
+    setMarkers(beaches);
+}
+function initializeMap(position) {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: position,
+        zoom: 16,
+    });
+    infoWindow = new google.maps.InfoWindow();
 
-// function initializeMap(position) {
-//     map = new google.maps.Map(document.getElementById("map"), {
-//         center: position,
-//         zoom: 16,
-//     });
-//     performSearch();
-//     const autocomplete = new google.maps.places.Autocomplete(
-//         document.getElementById("Region"),
-//         {
-//             types: ["geocode"],
-//             componentRestrictions: { country: "TW" },
-//         }
-//     );
+    setMarkers(beaches);
+}
 
-//     autocomplete.addListener("place_changed", function () {
-//         const place = autocomplete.getPlace();
-//         if (!place.geometry || !place.geometry.location) {
-//             return;
-//         }
-//         map.setCenter(place.geometry.location);
-//         map.setZoom(16);
-//         performSearch();
-//     });
+const cityDropdown = document.getElementById("Region");
+const clinicTypeDropdown = document.getElementById("Clinic");
+cityDropdown.addEventListener("change", updateMapMarkers, { passive: true });
+clinicTypeDropdown.addEventListener("change", updateMapMarkers, { passive: true });
 
 
-// }
 
-//     function performSearch() {
-//         const selectedRegion = regionSelect.value;
-//         const selectedClinicType = clinicTypeSelect.value;
-//         searchOnMap(clinicsData, selectedRegion, selectedClinicType);
-//     }
 
-//     function searchOnMap(clinicsData, region, clinicType) {
-//         markers.forEach(function (marker) {
-//             marker.setMap(null);
-//         });
-//         markers = [];
-//         const matchingClinics = clinicsData.filter((clinic) => {
-//             return clinic.region === region && clinic.type === clinicType;
-//         });
+function setMarkers(clinics) {
+    clearMarkers();
+    for (let i = 0; i < clinics.length; i++) {
+        const clinic = clinics[i];
+        const clinicName = clinic[0];
+        const latLng = { lat: clinic[1], lng: clinic[2] };
+        const marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: clinicName,
+        });
 
-//         matchingClinics.forEach((clinic) => {
-//             const marker = new google.maps.Marker({
-//                 position: clinic.location,
-//                 map: map,
-//                 title: clinic.name,
-//             });
+        marker.addListener("click", function (event) {
+            displayClinicInfo(clinic, event);
+        });
+        markers.push(marker);
+    }
+}
 
-//             marker.addListener("click", function () {
-//                 const infoWindow = new google.maps.InfoWindow({
-//                     content: `<strong>${clinic.name}</strong><br>地區: ${clinic.region}<br>類型: ${clinic.type}`,
-//                 });
-//                 infoWindow.open(map, marker);
-//             });
+function clearMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
 
-//             markers.push(marker);
-//         });
-//     }
 
-//     function setMarkers(map) {
-//         const image = {
-//             url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-//             size: new google.maps.Size(20, 32),
-//             origin: new google.maps.Point(0, 0),
-//             anchor: new google.maps.Point(0, 32),
-//         };
-//         const shape = {
-//             coords: [1, 1, 1, 20, 18, 20, 18, 1],
-//             type: "poly",
-//         };
+function displayClinicInfo(clinic, event) {
+    const clinicName = clinic[0];
+    const clinicType = clinic[3];
+    const clinicCity = clinic[4];
+    const clinicOfficalWebsite = clinic[5];
+    const clinicAddress = clinic[6];
+    const contentString = `
+        <div>
+            <h2>${clinicName}</h2>
+            <p>診所類型: ${clinicType}</p>
+            <p>所在地區: ${clinicCity}</p>
+            <p>官方網站: <a href="${clinicOfficalWebsite}" target="_blank">${clinicOfficalWebsite}</a></p>
+            <p>地址: ${clinicAddress}</p>
+        </div>
+    `;
+    infoWindow.setContent(contentString);
+    infoWindow.open(map, event.target); // 使用事件對象的目標作為參數
+}
 
-//         for (let i = 0; i < beaches.length; i++) {
-//             const beach = beaches[i];
+function updateMapMarkers() {
+    const selectedCity = cityDropdown.value;
+    const selectedClinicType = clinicTypeDropdown.value;
+    const filteredClinics = beaches.filter(clinic => {
+        const clinicCity = clinic[4];
 
-//             new google.maps.Marker({
-//                 position: { lat: beach[1], lng: beach[2] },
-//                 map,
-//                 icon: image,
-//                 shape: shape,
-//                 title: beach[0],
-//                 zIndex: beach[3],
-//             });
-//         }
-//     }
-// });
+        return (selectedCity === "All" || clinicCity === selectedCity) &&
+            (selectedClinicType === "All" || clinic[3] === selectedClinicType);
+    });
+
+    setMarkers(filteredClinics);
+
+    if (filteredClinics.length > 0) {
+        const firstClinic = filteredClinics[0];
+        const latLng = { lat: firstClinic[1], lng: firstClinic[2] };
+        map.setCenter(latLng);
+    }
+}
